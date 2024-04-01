121487b50a63f0f127f73006002270059d984776
Alaa Fedora <eng.alaa.janadi@gmail.com>
2024-04-01 03:05:20 +0200

    created maven spring boot app

---
+++ src/main/java/com/janadi/mavenspringbootjenkins/MavenSpringBootJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.janadi.mavenspringbootjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSpringBootJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSpringBootJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janadi/mavenspringbootjenkins/MavenSpringBootJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janadi.mavenspringbootjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenSpringBootJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
